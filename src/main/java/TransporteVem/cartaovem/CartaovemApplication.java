6ecb1d59d98e9a015d8d6769a84011a63ddd16b9
NatáliaCosta <naty1993ccs@gmail.com>
2022-10-25 22:45:34 -0300

    first commit

---
+++ src/main/java/TransporteVem/cartaovem/CartaovemApplication.java
@@ -0,0 +1,13 @@
+package TransporteVem.cartaovem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaovemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaovemApplication.class, args);
+	}
+
+}
